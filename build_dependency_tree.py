c8a7c5e6c089fca2b09d9963d2c3cf2be322089d
Garry Ingles <gingles@starry.com>
2022-01-22 22:02:37 -0500

    Continue initial file

--- build_dependency_tree.py
+++ build_dependency_tree.py
@@ -5,3 +5,8 @@ import sys
 from pathlib import Path
 
 args = [arg for arg in sys.argv]
+files_in_dir = [
+    [file for file in Path(root).glob('*')
+     if file.suffix in ('h', 'hpp', 'c', 'cpp')]
+    for root in args
+]
