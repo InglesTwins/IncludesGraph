4428f8daa153ff5a4fa84ccf44073a5c485e4e38
ominusliticus <elkevin111@gmail.com>
2022-01-22 22:15:45 -0500

    Added function that separates hdrs and impls
    Please see function comment for more detail:
    function name: separate_headers_and_implementations

--- build_dependency_tree.py
+++ build_dependency_tree.py
@@ -1,12 +1,27 @@
 """Main file to extract dependencies to model the include graph"""
 
-import os
 import sys
 from pathlib import Path
+from typing import List
 
 args = [arg for arg in sys.argv]
 files_in_dir = [
-    [file for file in Path(root).glob('*')
+    [file for file in Path(root).glob('*') # TODO: Make recursive search 
      if file.suffix in ('h', 'hpp', 'c', 'cpp')]
     for root in args
 ]
+
+def separate_headers_and_implementations(files_in_dir: List[Path]):
+    """ 
+    Separates files from current directory into header and implementation files.
+    
+    :param files_in_dir: List of paths to files C++ files in current directory
+    
+    return: returns: tuple of list containing paths to the header files in 
+    first slot and implementation files in the second
+    """
+    headers = [file for file in files_in_dir if file.suffix in ('h', 'hpp')]
+    impls = [file for file in files_in_dir if file.suffix in ('c', 'cpp')]
+    return headers, impls
+
+
