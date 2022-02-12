a7f43fbed6e5e550dde13badba4d0745003d31f6
ominusliticus <elkevin111@gmail.com>
2022-02-11 22:33:54 -0500

    Made python spec compilant and add output statement\n\nStarted with debugging for toy project, but this is currently\nincomplete.\nWe have managed to print a file, but its empyt.

--- build_dependency_tree.py
+++ build_dependency_tree.py
@@ -3,30 +3,52 @@
 import sys
 import re
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Union
 from typing import List, Dict
 
 
 def extract_files_from_directory():
     """FUnction recurses through the directory to find all file-like objects"""
 
+    def is_cpp_file(file: Union[Path, str]):
+        file_types = ('c', 'cpp', 'cxx', 'C', 'h', 'hpp', 'hxx')
+        return_value = False
+        for file_type in file_types:
+            return_value = return_value or file_type in str(file)
+            if return_value:
+                return True
+        return False
+
     def extract_files(dir_or_file: Path, list_o_files: List[Optional[Path]]):
         if dir_or_file.is_dir():
+            print(f'In dir: {str(dir_or_file.absolute())}')
             for path in dir_or_file.glob("*"):
                 extract_files(path, list_o_files)
         else:
-            list_o_files.append(dir_or_file)
+            if is_cpp_file(dir_or_file.name):
+                print(f'Appending file: {str(dir_or_file.absolute())}')
+                list_o_files.extend([dir_or_file])
+        return list_o_files
 
-    return [extract_files(Path(arg), []) for arg in sys.argv]
+    def flatten_list(lists: Optional[List[List]]):
+        return [
+                item for sublist in lists for item in sublist
+                if len(sublist) > 0]
+
+    return flatten_list(
+            [extract_files(Path(arg), []) for arg in sys.argv])
+
 
 # I think we can get rid of thise function
 def separate_headers_and_implementations(files_in_dir: List[Path]):
-    """ 
-    Separates files from current directory into header and implementation files.
-    
-    :param files_in_dir:    List of paths to files C++ files in current directory
-    return: returns:        tuple of list containing paths to the header files in 
-    first slot and implementation files in the second
+    """
+    Separates files from current directory into header and implementation files
+
+    :param files_in_dir:    List of paths to files C++ files in current
+                            directory
+    return: returns:        tuple of list containing paths to the header files
+                            in first slot and implementation files in the
+                            second
     """
     headers = [file for file in files_in_dir if file.suffix in ('h', 'hpp')]
     impls = [file for file in files_in_dir if file.suffix in ('c', 'cpp')]
@@ -37,7 +59,9 @@ def separate_headers_and_implementations(files_in_dir: List[Path]):
 # KI: No, but also don't want it to be a global variable. This screams classes
 #
 # match either the pattern `<...>` or `"..."`
-include_statement_pattern = re.compile
+include_statement_pattern = re.compile(r"<.+?>|\".+?\"")
+
+
 def create_single_file_dependency_list(file: Union[Path, str]):
     """
     Go through header files and implementation files and scan for `#include`
@@ -47,16 +71,16 @@ def create_single_file_dependency_list(file: Union[Path, str]):
 
     With C++20 and C++23 we will have to include import statements as well
     But we leave this for future work
-    
+
     :param file:        path to C++ file in current project
     return: returns:    a list of the files that `file` depends on
     """
-    with open(file) as f:
-        # some files may have macros that check for operatoring system or 
+    with open(str(file.absolute())) as f:
+        # some files may have macros that check for operatoring system or
         # compiler compatibilities. For now we ignore these as well.
-        # We also need to ignore include statements made in comments 
+        # We also need to ignore include statements made in comments
         include_statements = []
-        b_is_block_comment = False # Lines that start with /*
+        b_is_block_comment = False  # Lines that start with /*
         for line in f.readlines():
             if b_is_block_comment:
                 if line.split()[0] == '*/':
@@ -64,57 +88,65 @@ def create_single_file_dependency_list(file: Union[Path, str]):
                 else:
                     continue
             else:
-                if line.contains('/*'):
+                if '/*' in line:
                     b_is_block_comment = True
                     continue
-                elif line.strip()[0:2] == '//':
+                elif line.strip(' ')[0:2] == '//':
                     continue
-                elif line.split()[0] in ('#include', '%:include'):
-                    # we always expect the included file to be given second (required
-                    # by standards)
+                elif line.replace(' ', '')[0:8] == '#include' or \
+                        line.replace(' ', '')[0:9] == '%:include':
+                    # we always expect the included file to be given second
+                    # (required by standards)
                     re_match = include_statement_pattern.match(line.split()[1])
                     # We only expect one entry in the list
-                    if re_match: # first matching group is empty
+                    if re_match:  # first matching group is empty
+                        print(f'No header files found: {file}')
                         continue
+                    elif not re_match.group(1):
+                        # Check if match with `<...>` is empty
+                        print(f"""Header file found
+                                {str(file.absolute())}:{re_match.group(2)}""")
+                        include_statements.append(re_match.group(2))
                     else:
-                        if not re_match.group(1): # Check if match with `<...>` is empty
-                            include_statements.append(re_match.group(2))
-                        else: # since we have a match and first group is non-empty we
-                              # apend
-                            include_statements.append(re_match.group(1))
-                    else:
-                        continue
+                        # since we have a match and first group is non-empty we
+                        # apend
+                        print(f"""Header file found
+                                {str(file.absolute())}:{re_match.group(1)}""")
+                        include_statements.append(re_match.group(1))
         return include_statements
 
 
 def output_dependency_tree_to_dot_file(
-        dep_tree: Dict[List[Union[Path, str]]], 
-        output_name: Optional[Union[Path, str]]
-    ):
+        dep_tree: Dict[List[Union[Path, str]], str],
+        output_name: Optional[Union[Path, str]]):
     """
-    Goes through dictionary of files gathered from recursive search of 
-    directory and prints all the nodes it will be attached to in file 
+    Goes through dictionary of files gathered from recursive search of
+    directory and prints all the nodes it will be attached to in file
     named output_name.
-    Output is formated to be compatible with the DOT command line program: 
+    Output is formated to be compatible with the DOT command line program:
     https://graphviz.org/doc/info/lang.html
 
-    :param dep_tree:    dictionary with files in project as keys and list 
+    :param dep_tree:    dictionary with files in project as keys and list
                         of their dependencies as value
-    :param output_name: name of output file or path to output file 
+    :param output_name: name of output file or path to output file
     """
     if not isinstance(dep_tree, dict):
-        # For cleaner debugging messages, only keep while still developing program
-        print("None dictionary object passed.")
+        # For cleaner debugging messages, only keep while still developing
+        # program
+        print("None dictionary object passed. Exiting...")
         quit()
 
     if not output_name:
-        output_name = 'includes_tree_output.dot'
-    
-    # TODO: Need to colorize output. 
+        output_name = Path('./includes_tree_output.dot').absolute()
+
+    print(f'Output file is: {str(output_name)}')
+
+    # TODO: Need to colorize output.
     #       E.g. header files are blue, translation units red
     #       Standard Template Library files are different shaped bubbles, etc
-    #       Of course this will require a key of some sort to be documentd somewhere
-    with open(output_name) as f:
+    #       Of course this will require a key of some sort to be documentd
+    #       somewhere
+    with open(str(output_name.absolute()), 'w') as f:
         f.write("graph {\n")
         for key, dep_list in dep_tree.items():
             for entry in dep_list:
@@ -122,22 +154,36 @@ def output_dependency_tree_to_dot_file(
         f.write("}")
 
 
-def generate_dependency_tree(files_in_project: List[Path], keep_std_files: bool=True):
+def generate_dependency_tree(
+        files_in_project: List[Path],
+        keep_std_files: bool = True):
     """
-    Creates a dictionary with file name as key and its dependency tree as the value
+    Creates a dictionary with file name as key and its dependency tree as the
+    value
 
-    :param files_in_project:    all C++ files in provided directory and subdirectories
-    :param keep_std_files:      flag to determine whether to track C/C++ standarad
+    :param files_in_project:    all C++ files in provided directory and
+                                subdirectories
+    :param keep_std_files:      flag to determine whether to track C/C++
+                                standarad
                                 files
 
     :returns return: dictionary of files which project specific files depend on
     """
-    with open("utils/C_std_headers.txt",'r') as f:
-        C_std_files = [line.strip() for line in f.readlines()] # strip to remove '\n' char
-    with open("utils/Cpp_std_headers.txt",'r') as f:
-        Cpp_std_files = [line.strip() for line in f.readlines()]
+    pwd = Path(__file__).absolute().parent
+    with open(f"{str(pwd)}/util/C_std_headers.txt", 'r') as f:
+        c_std_files = [
+                line.strip()
+                for line in f.readlines()
+                if line
+                ]  # strip to remove '\n' char
+    with open(f"{str(pwd)}/util/Cpp_std_headers.txt", 'r') as f:
+        cpp_std_files = [
+                line.strip() 
+                for line in f.readlines()
+                if line]
     return dict(
                 (file, create_single_file_dependency_list(file))
                 for file in files_in_project
-                if (not file in [*C_std_files, *Cpp_std_fiels]) or keep_std_files # complicated logica D=
+                if (file not in [*c_std_files, *cpp_std_files])
+                or keep_std_files  # complicated logic
             )
