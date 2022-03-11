9d715a9abe5a3f7cd692848769e8377d49d6e759
ominusliticus <elkevin111@gmail.com>
2022-03-11 00:04:52 -0500

    Test script: Modified so that current directory is hardcoded
    This is done to avoid passing command line arguments.
    Once the parsers is done, this file will match

--- test_dir/test_1.py
+++ test_dir/test_1.py
@@ -7,7 +7,8 @@ sys.path.append(directory.parent.parent.__str__())
 import build_dependency_tree as ig
 
 if __name__ == '__main__':
-    extracted_files = ig.extract_files_from_directory()
+    print(directory.parent)
+    extracted_files = ig.extract_files_from_directory([directory.parent])
     ig.output_dependency_tree_to_dot_file(
             ig.generate_dependency_tree(extracted_files),
             Path("./test_1.dot"))
