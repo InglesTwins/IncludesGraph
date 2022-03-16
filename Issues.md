7bf1a14b949908912a3f5dc387b6b839437e2861
ominusliticus <elkevin111@gmail.com>
2022-03-16 01:23:24 -0400

    DOT output: Continue parsing file paths for output
    When outputting the dependency tree to the DOT files, the program
    was now smart enough to realize that translation units in the same
    directory do not get the full path to the file and would be dupli-
    cated in the graph.
    
    This commit is the first to properly document the efforts the past
    few commits have been trying to remedy. The only problem that rem-
    ains now is to resolve include statements with relative paths.
    
    I have add a Markdown file to help keep track of the ongoing issu-
    es, as opposed to openning an issue on GitHub like I should.

---
+++ Issues.md
@@ -0,0 +1,4 @@
+## March 15, 2022
+<input type="checkbox"> Remove file paths attached to std library files </input>  
+<input type="checkbox"> Have pathlib library parse the relative directories </input>  
+<input type="checkbox"> Remove leading front slash from output </input>  
---
+++ test_1.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import sys
+
+directory = Path(__file__).absolute()
+sys.path.append(directory.parent.parent.__str__())
+
+import build_dependency_tree as ig
+
+if __name__ == '__main__':
+    print(directory.parent)
+    extracted_files, set_o_dirs = ig.extract_files_from_directory([directory.parent])
+    ig.output_dependency_tree_to_dot_file(
+            ig.generate_dependency_tree(extracted_files),
+            set_o_dirs,
+            Path("./test_1.dot"))
